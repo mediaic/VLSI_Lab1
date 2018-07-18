ed75d40d297b64217b2785973d7d5f4057f63715
johnjohnlin <johnjohnlys@gmail.com>
2018-07-18 18:58:40 +0800

    Update timing simulation

--- sim/Rgb888ToYuv422_test.sv
+++ sim/Rgb888ToYuv422_test.sv
@@ -10,21 +10,21 @@ module Rgb888ToYuv422_test;
 logic clk, rst;
 `Pos(rst_out, rst)
 `ifdef SYN
-`PosIfDelayed(ck_ev, clk, rst, 0.1)
+`PosIfDelayed(ck_ev, clk, rst, 1)
 `else
 `PosIf(ck_ev, clk, rst)
 `endif
 `WithFinish
 
-always #1 clk = ~clk;
+always #5 clk = ~clk;
 initial begin
 	$fsdbDumpfile("Rgb888ToYuv422_test.fsdb");
 	$fsdbDumpvars(0, Rgb888ToYuv422_test, "+mda");
 	clk = 0;
 	rst = 1;
 	#1 $NicotbInit();
-	#11 rst = 0;
-	#10 rst = 1;
+	#31 rst = 0;
+	#30 rst = 1;
 	#20000 $display("Timeout");
 	$NicotbFinal();
 	$finish;
